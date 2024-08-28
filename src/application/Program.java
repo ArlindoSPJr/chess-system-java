9c9b8b0798f7d330e96fc39d73ebf1d99a42d3a1
Arlindo Junior <arlindo.junior@sga.pucminas.br>
2024-08-28 16:13:02 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,10 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("oi");
-        System.out.println("oi2");
+        Position position = new Position(3,5);
+        System.out.println(position);
     }
 }
